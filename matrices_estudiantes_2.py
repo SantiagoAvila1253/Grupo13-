ae0c3d3b60d34c8bfe534919f11a60f61a9a4d4f
DKuade <dkatz@uade.edu.ar>
2025-08-23 13:35:34 -0300

    actualizar listas y matrices, crear matriz presentismo, comentar, documentar y oordenar el código

--- matrices_estudiantes_2.py
+++ matrices_estudiantes_2.py
@@ -1,62 +1,55 @@
+# funciones
 
-# matrices
-
-Alumnos = [
-    [1, "Juan", "Saltid", "08-09-2007", "jsaltid@uade.edu.ar"],
-    [2, "Pablo", "Altanio", "19-04-2004", "paltanio@uade.edu.ar"],
-    [3, "Marcelo", "Mandeo", "02-12-2007", "mmandeo@uade.edu.ar"],
-    [4, "Mateo", "Fernandez", "16-03-2005", "mfernandez@uade.edu.ar"],
-    [5, "Lucia", "Castro", "17-07-2006", "lucastro@uade.edu.ar"],
-    [6, "Julian", "Martinez", "06-05-2003", "jmartinez@uade.edu.ar"],
-    [7, "Sofia", "Nando", "13-07-2004", "snando@uade.edu.ar"],
-    [8, "Agustina", "Gonzales", "09-03-2007", "agonzales@uade.edu.ar"],
-    [9, "Franchesca", "Ameglio", "11-09-2006", "fameglio@uade.edu.ar"],
-    [10, "Joaquin", "Ayala", "20-08-2007", "jayala@uade.edu.ar"],
-    [11, "Valentina", "Catillo", "01-02-2005", "vcastillo@uade.edu.ar"],
-    [12, "Lautaro", "Nuñez", "18-04-2006", "lnuñez@uade.edu.ar"],
-    [13, "Florencia", "Espinosa", "30-01-2007", "fespinosa@uade.edu.ar"],
-    [14, "Tomas", "Fuentes", "23-03-2004", "tfuentes@uade.edu.ar"],
-    [15, "Faustina", "Flores", "12-08-2006", "fflores@uade.edu.ar"],
-]
-
-materia = ["Matemática", "Martes" "14:00 - 18:00"]
-
-clases = [
-    [1, "2025-08-05"],
-    [2,  "2025-08-12"],
-    [3,  "2025-08-19"],
-    [4,  "2025-08-26"],
-    [5,  "2025-09-02"],
-    [6,  "2025-09-09"],
-    [7,  "2025-00-16"],
-    [8,  "2025-09-23"],
-    [9,  "2025-09-30"],
-    [10,  "2025-10-07"],
-    [11,  "2025-10-14"],
-    [12,  "2025-10-21"],
-    [13,  "2025-10-28"],
-    [14,  "2025-11-04"],
-    [15,  "2025-11-11"],
-    [16,  "2025-11-18"],
-    [17,  "2025-11-25"],
-]
-
-asistencias = [
-    [1, 1, "Presente"],
-    [2, 1, "Ausente"],
-    [3, 2, "Presente"],
-    [4, 4, "presente"],
-    [5, 3, "ausente"],
-]
-
-
+# función imprimir matrices -- actualizar
 def mostrar_matriz(nombre, matriz):
     print(f"{nombre}:")
     for fila in matriz:
         print(fila)
+    
+    return 
+# matrices
 
+# matriz materias
+materia = [["Matemática", "Martes", "14:00 - 18:00"]]
 
-#Programa principal 
+# lista clases
+clases = ["2025-08-05", "2025-08-12", "2025-08-19", "2025-08-26", "2025-09-02", "2025-09-09", "2025-09-16", "2025-09-23", "2025-09-30", "2025-10-07", "2025-10-14", "2025-10-21", "2025-10-28", "2025-11-04", "2025-11-11", "2025-11-18", "2025-11-25"]
+
+# matriz alumnos
+Alumnos = [
+    ["Juan", "Saltid", "08-09-2007", "jsaltid@uade.edu.ar"],
+    ["Pablo", "Altanio", "19-04-2004", "paltanio@uade.edu.ar"],
+    ["Marcelo", "Mandeo", "02-12-2007", "mmandeo@uade.edu.ar"],
+    ["Mateo", "Fernandez", "16-03-2005", "mfernandez@uade.edu.ar"],
+    ["Lucia", "Castro", "17-07-2006", "lucastro@uade.edu.ar"],
+    ["Julian", "Martinez", "06-05-2003", "jmartinez@uade.edu.ar"],
+    ["Sofia", "Nando", "13-07-2004", "snando@uade.edu.ar"],
+    ["Agustina", "Gonzales", "09-03-2007", "agonzales@uade.edu.ar"],
+    ["Franchesca", "Ameglio", "11-09-2006", "fameglio@uade.edu.ar"],
+    ["Joaquin", "Ayala", "20-08-2007", "jayala@uade.edu.ar"],
+    ["Valentina", "Catillo", "01-02-2005", "vcastillo@uade.edu.ar"],
+    ["Lautaro", "Nuñez", "18-04-2006", "lnuñez@uade.edu.ar"],
+    ["Florencia", "Espinosa", "30-01-2007", "fespinosa@uade.edu.ar"],
+    ["Tomas", "Fuentes", "23-03-2004", "tfuentes@uade.edu.ar"],
+    ["Faustina", "Flores", "12-08-2006", "fflores@uade.edu.ar"]
+]
+
+# lista asistencia
+asistencias = ["Presente", "Ausente justificado", "Ausente injustificado"]
+
+presentismo = [
+    [1,1,1,1],
+    [1,1,2,1],
+    [1,1,3,1],
+    [1,1,4,1],
+    [1,2,1,1],
+    [1,2,2,1],
+    [1,2,3,2],
+    [1,2,4,1]
+]
+
+#Programa principal
+
 mostrar_matriz("Alumnos", Alumnos,)
 mostrar_matriz("Materia", materia,)
 mostrar_matriz("Clases", clases,)
